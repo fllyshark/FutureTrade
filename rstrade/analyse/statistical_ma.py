fa50b416922d44e100db057e3ba97e3b0d45b0c2
wly <787115719@qq.com>
2018-10-10 23:30:44 +0800

    代码提交，版本：0.02

---
+++ rstrade/analyse/statistical_ma.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+# -*- coding: utf-8 -*-
+# @Time    : 2018/10/10 15:46
+# @Author  : wly
+# @Email   : 787115719@qq.com
+# @File    : statistical_ma.py
+# @Software: PyCharm
+"""
--- rstrade/jqdata/rs_conn_jqdata.py
+++ rstrade/jqdata/rs_conn_jqdata.py
@@ -14,9 +14,9 @@ import pandas as pd
 from threading import Timer
 import matplotlib.pyplot as plt
 import datetime
-from wxpy import *
 from rstrade.jqdata import rs_fut_const as rs_const
-
+from rstrade.util import indictor as indi
+from rstrade.util import fileUtils as fileu
 from jqdatasdk import *
 
 def _join_contracts(contracts, startdate, endday, freq):
@@ -30,26 +30,39 @@ def _join_contracts(contracts, startdate, endday, freq):
     :param freq: 计算周期
     :return:
     """
-    hsdata = get_price(rs_const.BLACK_MAIN[contracts], start_date=startdate, end_date=endday, frequency=freq,
+    hsdata = get_price(rs_const.CONTRACTS_MAIN[contracts], start_date=startdate, end_date=endday, frequency=freq,
                        skip_paused=True)
     data_len = range(len(hsdata))
+    if len(hsdata)<=0:
+        print("%s--该合约无数据！exit--\n"%rs_const.CONTRACTS_MAIN[contracts])
+        return
     base_price = hsdata['close'][0]
-    plt.plot(data_len, hsdata['close'] / base_price, rs_const.BLACK_LINECOLOR[contracts], label=contracts)
+    #plt.plot(data_len, hsdata['close'] / base_price, rs_const.BLACK_LINECOLOR[contracts], label=contracts)
+    plt.plot(data_len, hsdata['close'] / base_price, label=contracts)
+    malast=indi.ma(hsdata,50)
+
+    if hsdata['close'][-1]>malast[-1]:
+       strclc=("%s收盘价大于50周期线\n"%contracts)
+       #print(rs_const.FILEPATH['clc_file'])
+       fileu.appendStrToFile(rs_const.clc_filepath%(datetime.date.today().strftime('%Y-%m-%d')),strclc)
+
     return
 
 
 auth('15916406969','a456789')#依次输入账号、密码，链接到平台数据库
 
-startdate='2018-07-27'
+startdate='2018-05-27'
 freq='1d'
 endday = datetime.date.today()
 
-plt.figure()
+plt.figure(figsize=(9, 9.5))
 
-_join_contracts('MA',startdate,endday,freq)
-_join_contracts('RB',startdate,endday,freq)
-_join_contracts('HC',startdate,endday,freq)
-_join_contracts('J',startdate,endday,freq)
+#_join_contracts('MA',startdate,endday,freq)
+#_join_contracts('RB',startdate,endday,freq)
+#_join_contracts('HC',startdate,endday,freq)
+#_join_contracts('J',startdate,endday,freq)
+for index in rs_const.CONTRACTS_MAIN:
+    _join_contracts(index, startdate, endday, freq)
 #plt.text(4, 5, 6, ha='center', va='bottom', fontsize=20)
 
 plt.title("future line")
@@ -66,13 +79,5 @@ plt.legend(loc=1,ncol=2)
 
 plt.show()
 
-bot =Bot(cache_path=True)
-#bot.file_helper.send("hello")
-def sendNews():
-    t = Timer(30, bot.file_helper.send("hello"))
-    t.start()
-sendNews()
-if __name__ == "__main__":
-    sendNews()
 
 
